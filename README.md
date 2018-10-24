98352e44e5849ee072bc51a1fde3d61972d77ac6
CAM <34009133+CMichael82@users.noreply.github.com>
2018-10-23 19:22:48 -0600

    working app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hot-restaurant
+
+University of Utah Boot-Camp in-class group activity to practice Express.
--- server.js
+++ server.js
@@ -2,13 +2,28 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var tables = [];
-var waiting = [];
+var tables = require("./tablesData.js");
+var waiting = require("./waitingData.js");
+
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/tables", function(req, res) {
+    res.json(tables);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waiting);
+  });
+
 
 
 app.get("/", function(req, res) {
@@ -19,8 +34,8 @@ app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/api/tables", function(req, res) {
-  return res.json(tables);
+app.get("/tables", function(req, res) {
+  res.sendfile(path.join(__dirname, "tables.html"));
 });
 
 // // Displays a single character, or returns false
@@ -39,7 +54,7 @@ app.get("/api/tables", function(req, res) {
 // });
 
 
-app.post("/api/TABLES", function (req, res) {
+app.post("/api/tables", function (req, res) {
 	var newTable = req.body;
 	// newTable.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 	console.log(newTable);
